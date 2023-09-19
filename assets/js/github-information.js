ecbbe80d911dd8aa525b5cba112be37f546cee15
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-09-19 15:02:46 +0000

    Add the variable to display if the user makes too many requests

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -60,6 +60,9 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for the user ${username}</h2>`)
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Data(errorResponse.getResponseHeader('X-RateLimit-Reset')* 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocalDateSring()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
